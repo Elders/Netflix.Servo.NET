ebe575cfc96a76f7c64c27e6f17cc300a3bc0520
mynkow <mynkow@gmail.com>
2015-10-08 17:48:57 +0300

    NumericMonitor + Gauge

---
+++ src/Netflix.Servo/Monitor/Gauge.cs
@@ -0,0 +1,10 @@
+﻿namespace Netflix.Servo.Monitor
+{
+    /// <summary>
+    /// Monitor type that provides the current value, e.g., the percentage of disk space used.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface Gauge<T> : NumericMonitor<T>
+    {
+    }
+}
---
+++ src/Netflix.Servo/Monitor/NumericMonitor.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Netflix.Servo.Monitor
+{
+
+    /// <summary>
+    /// A monitor type that has a numeric value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface NumericMonitor<T> : Monitor<T>
+    {
+    }
+}

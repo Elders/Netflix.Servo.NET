1c27e2586c2c207a57557f78eeee659bbbba1d55
mynkow <mynkow@gmail.com>
2015-10-08 17:03:18 +0300

    Monitor

---
+++ src/Netflix.Servo/Class1.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Netflix.Servo
+//{
+//    /**
+// * Registry to keep track of objects with
+// * {@link com.netflix.servo.annotations.Monitor} annotations.
+// */
+//    public interface MonitorRegistry
+//    {
+//        /**
+//         * The set of registered Monitor objects.
+//         */
+//        Collection<Monitor<?>> getRegisteredMonitors();
+
+//        /**
+//         * Register a new monitor in the registry.
+//         */
+//        void register(Monitor<?> monitor);
+
+//        /**
+//         * Unregister a Monitor from the registry.
+//         */
+//        void unregister(Monitor<?> monitor);
+
+//        /**
+//         * Check whether a monitor has been registerd.
+//         */
+//        boolean isRegistered(Monitor<?> monitor);
+//    }
+//}
---
+++ src/Netflix.Servo/Monitor/Monitor.cs
@@ -0,0 +1,25 @@
+﻿namespace Netflix.Servo.Monitor
+{
+    /**
+ * Provides a way to sample a value tied to a particular configuration.
+ */
+    public interface Monitor<T>
+    {
+
+        /**
+         * Returns the current value for the monitor for the default polling interval.
+         */
+        T getValue();
+
+        /**
+         * Returns the current value for the monitor for the nth poller.
+         */
+        T getValue(int pollerIndex);
+
+
+        /**
+         * Configuration used to identify a monitor and provide metadata used in aggregations.
+         */
+        MonitorConfig getConfig();
+    }
+}

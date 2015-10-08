31828eb6789daa455180c5b8c9b9072040b7415c
mynkow <mynkow@gmail.com>
2015-10-08 17:44:42 +0300

    AbstractMonitor

---
+++ src/Netflix.Servo/Monitor/AbstractMonitor.cs
@@ -0,0 +1,34 @@
+﻿using Netflix.Servo.Util;
+
+namespace Netflix.Servo.Monitor
+{
+    /// <summary>
+    ///  Base type to simplify implementing monitors.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class AbstractMonitor<T> : Monitor<T>
+    {
+        protected MonitorConfig config;
+
+        /// <summary>
+        /// Create a new instance with the specified configuration.
+        /// </summary>
+        /// <param name="config"></param>
+        protected AbstractMonitor(MonitorConfig config)
+        {
+            this.config = Preconditions.checkNotNull(config, "config");
+        }
+
+        public MonitorConfig getConfig()
+        {
+            return config;
+        }
+
+        public T getValue()
+        {
+            return getValue(0);
+        }
+
+        public abstract T getValue(int pollerIndex);
+    }
+}

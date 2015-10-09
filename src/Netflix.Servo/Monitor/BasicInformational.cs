efc723bdefbe9eb89b19e35f01098d4bad61308c
mynkow <mynkow@gmail.com>
2015-10-09 14:27:01 +0300

    Informational + BasicInformational

---
+++ src/Netflix.Servo/Monitor/BasicInformational.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Java.Util.Concurrent.Atomic;
+using Netflix.Servo.Attributes;
+
+namespace Netflix.Servo.Monitor
+{
+    /**
+  * A simple informational implementation that maintains a string value.
+  */
+    public class BasicInformational : AbstractMonitor<String>, Informational
+    {
+        private readonly AtomicReference<String> info = new AtomicReference<string>();
+
+        /**
+         * Creates a new instance of the counter.
+         */
+        public BasicInformational(MonitorConfig config)
+            : base(config.withAdditionalTag(DataSourceType.INFORMATIONAL))
+        {
+
+        }
+
+        /**
+         * Set the value to show for this monitor.
+         */
+        public void setValue(String value)
+        {
+            info.GetAndSet(value);
+        }
+
+        public override String getValue(int pollerIndex)
+        {
+            return info.Value;
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (o == null || !(o is BasicInformational))
+            {
+                return false;
+            }
+            BasicInformational that = (BasicInformational)o;
+
+            String thisInfo = info.Value;
+            String thatInfo = that.info.Value;
+            return config.Equals(that.config)
+                && (thisInfo == null ? thatInfo == null : thisInfo.Equals(thatInfo));
+        }
+
+        public override int GetHashCode()
+        {
+            int result = config.GetHashCode();
+            int infoHashcode = info.Value != null ? info.Value.GetHashCode() : 0;
+            result = 31 * result + infoHashcode;
+            return result;
+        }
+
+        public override String ToString()
+        {
+            return "BasicInformational{config=" + config + ", info=" + info + '}';
+        }
+    }
+}
---
+++ src/Netflix.Servo/Monitor/Informational.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Netflix.Servo.Monitor
+{
+    /**
+ * Monitor with a value type of string.
+ */
+    public interface Informational : Monitor<String>
+    {
+    }
+}

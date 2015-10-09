78fe8f5141ba66a77f47fdb85d7953590e3ab0e1
mynkow <mynkow@gmail.com>
2015-10-09 14:08:25 +0300

    BasicCounter + Counter

---
+++ src/Netflix.Servo/Monitor/Counter.cs
@@ -0,0 +1,18 @@
+﻿namespace Netflix.Servo.Monitor
+{
+    /**
+ * Monitor type for tracking how often some event is occurring.
+ */
+    public interface Counter : NumericMonitor<object>
+    {
+        /**
+         * Update the count by one.
+         */
+        void increment();
+
+        /**
+         * Update the count by the specified amount.
+         */
+        void increment(long amount);
+    }
+}

fd2566601498e4e819d18efadd72d47de7ea04dc
Sinstraliz <blagovest.vp@gmail.com>
2015-10-09 12:21:00 +0300

    Converted annotations to .net attributes.

---
+++ src/Netflix.Servo/Attributes/DataSourceLevel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Netflix.Servo.Tag;
+
+namespace Netflix.Servo.Attributes
+{
+    public enum DataSourceLevel
+    {
+        DEBUG,
+        INFO,
+        CRITICAL
+    }
+
+    /**
+ * Indicates a level for the monitor. This is meant to be similar to log levels to provide a
+ * quick way to perform high-level filtering.
+ */
+    public class DataSourceLevelTag : ITag
+    {
+        private DataSourceLevelTag(string name)
+        {
+            this.name = name;
+        }
+
+        /**
+         * Fine granularity monitors that provide a high amount of detail.
+         */
+        public static DataSourceLevelTag DEBUG = new DataSourceLevelTag(nameof(DataSourceLevel.DEBUG));
+
+        /**
+         * The default level for monitors.
+         */
+        public static DataSourceLevelTag INFO = new DataSourceLevelTag(nameof(DataSourceLevel.INFO));
+
+        /**
+         * Most important monitors for an application.
+         */
+        public static DataSourceLevelTag CRITICAL = new DataSourceLevelTag(nameof(DataSourceLevel.CRITICAL));
+
+        /**
+         * Key name used for the data source level tag.
+         */
+        public static String KEY = "level";
+        public string name = "default";
+
+        /**
+         * {@inheritDoc}
+         */
+        public String getKey()
+        {
+            return KEY;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public String getValue()
+        {
+            return name;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public String tagString()
+        {
+            return getKey() + "=" + getValue();
+        }
+    }
+}
---
+++ src/Netflix.Servo/Attributes/Monitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Netflix.Servo.Attributes
+{
+    /**
+ * Annotation indicating a field or method should be collected for monitoring.
+ * The attributes annotated should be thread-safe for access by a background
+ * thread. If a method is annotated it should be inexpensive and avoid any
+ * potentially costly operations such as IO and networking. Expect that the
+ * fields will be polled frequently and cache values that require expensive
+ * computation rather than computing them inline.
+ */
+    //@Documented
+    //@Retention(RetentionPolicy.RUNTIME)
+    //@Target({ ElementType.FIELD, ElementType.METHOD })
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method)]
+    public class MonitorAttribute : Attribute
+    {
+        /**
+         * Name of the annotated attribute.
+         */
+        public String name { get; set; } = default(string);
+
+        /**
+         * Type of value that is annotated, for more information see
+         * {@link DataSourceType}.
+         */
+        public DataSourceType type { get; set; } = DataSourceType.INFORMATIONAL;
+
+        /**
+         * Level of the value that is annotated, for more information see
+         * {@link DataSourceLevel}.
+         */
+        public DataSourceLevel level { get; set; } = DataSourceLevel.INFO;
+
+        /**
+         * A human readable description of the annotated attribute.
+         */
+        public String description { get; set; } = default(string);
+    }
+}

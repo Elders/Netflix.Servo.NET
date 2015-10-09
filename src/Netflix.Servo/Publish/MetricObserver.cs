65dd9bee77f819fecd004bc19b5d399aae78865f
Sinstraliz <blagovest.vp@gmail.com>
2015-10-09 13:56:21 +0300

    Fixed .gitignore

---
+++ src/Netflix.Servo/Publish/MetricObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Netflix.Servo.Publish
+{
+    public interface MetricObserver
+    {
+        /**
+         * Invoked with the most recent values for a set of metrics.
+         */
+        void update(List<Metric> metrics);
+
+        /**
+         * Name associated with an observer. Mostly used to make log messages more
+         * informative.
+         */
+        String getName();
+    }
+}
